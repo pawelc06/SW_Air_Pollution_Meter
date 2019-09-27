a8e677c1cb3cf428bce5a5942218404ddab37de9
Pawel <pawel.czop@gmail.czop>
2019-09-27 18:59:27 +0200

    added inc

---
+++ inc/defines.h
@@ -0,0 +1,16 @@
+/*
+ * defines.h
+ *
+ *  Created on: 4 paü 2017
+ *      Author: Pawe≥
+ */
+
+#ifndef SRC_DEFINES_H_
+#define SRC_DEFINES_H_
+
+
+
+/* Activate SDIO 4-bit mode */
+#define FATFS_SDIO_4BIT 		1
+
+#endif /* SRC_DEFINES_H_ */
